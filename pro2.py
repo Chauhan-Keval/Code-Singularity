117882bfd25c9de8fce57d9c5111d372b4b1bc9d
Chauhan-Keval <kevalk292@gmail.com>
2025-02-03 13:03:56 +0530

    vv

---
+++ pro2.py
@@ -0,0 +1,8 @@
+a = 20
+b = 10
+def ari():
+    print('Add =',a+b)
+    print('Sub =',a-b)
+    print('Sub =',a*b)
+    print('Sub =',a/b)
+ari()
---
+++ pro3.py
@@ -0,0 +1,8 @@
+def pel():
+    a=input("Enter a value :")
+    b=a[::-1]
+    if (a==b):
+        print("pelindrome")
+    else:
+        print("Not pelindrome")
+pel()
